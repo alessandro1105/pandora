1cdb961825da27c15095c2a74c30ab433d6a9b97
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2018-12-29 00:54:36 +0100

    Started developing webapp
    - Splitted main template in page modules
    - Created homepage temporary content
    - Created error 404 module and template
    - Added ui-router vendor file
    - Added ngResource vendor file
    - Added ui-bootstrap vendor file

--- webapp/src/app/app.module.js
+++ webapp/src/app/app.module.js
@@ -1,4 +1,29 @@
 angular
     .module('pandora', [
+        'ui.router', // UI Router
 
-    ]);
+        'page', // Page Module
+        'homepage', // Homepage Module
+        'error404' // Error 404 Module
+    ])
+
+    .config(function ($urlRouterProvider, $locationProvider, $urlMatcherFactoryProvider) {
+        // UI Router non strict mode and case insensitive
+        $urlMatcherFactoryProvider.caseInsensitive(true);
+        $urlMatcherFactoryProvider.strictMode(false);
+
+        // Enabling HTML5 MURLs
+        $locationProvider.html5Mode(true);
+
+        // Automatically redirect to homepage at startup
+        $urlRouterProvider
+            .when('/', function ($state) {
+                $state.go('homepage');
+            })
+            .otherwise(function ($injector) {
+                // Get $state service from $injector
+                $state = $injector.get("$state");
+                // Transit to error404 state
+                $state.go('error404');
+            });
+    });
---
+++ webapp/src/app/components/homepage/homepage.module.js
@@ -0,0 +1,26 @@
+// Page module
+angular
+    .module('homepage', [
+
+    ])
+
+    .config(function ($stateProvider, $locationProvider, $urlMatcherFactoryProvider) {
+        // UI Router non strict mode and case insensitive
+        $urlMatcherFactoryProvider.caseInsensitive(true);
+        $urlMatcherFactoryProvider.strictMode(false);
+
+        // Enabling HTML5 MURLs
+        $locationProvider.html5Mode(true);
+
+        // Sidebar container state (Used only to place the sidebar container tpl on the page)
+        $stateProvider
+            .state({
+                name: 'homepage',
+                url: '/',
+                parent: 'page',
+                templateUrl: 'app/components/homepage/homepage.tpl.html'
+            });
+
+    });
+
+
