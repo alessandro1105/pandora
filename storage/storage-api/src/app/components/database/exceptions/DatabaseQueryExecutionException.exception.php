8c22b58609f7ee665adfe82b53242b98f293a488
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2019-01-27 00:58:24 +0100

    Added Database Service to access database
    - Added Database Service to access database

---
+++ storage/storage-api/src/app/components/database/exceptions/DatabaseQueryExecutionException.exception.php
@@ -0,0 +1,10 @@
+<?php
+
+    namespace App\Components\Database\Exceptions;
+
+    use \Exception;
+
+    // The connection to the database cannot be performed
+    class DatabaseQueryExecutionException extends Exception {
+
+    }
